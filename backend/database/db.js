cdaa14a551bf70b0afe2946034ae1d4518be1618
segun <soshice@gmail.com>
2023-04-15 20:02:37 +0100

    Another backend dep

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,5 +1,5 @@
 const {MongoClient} = require("mongodb")
-const url = "mongodb://localhost:27017"
+const url = "https://segblog-backend.onrender.com/"
 const client = new MongoClient(url)
 
 const dbName = "my_blog"
